8f1cdb1e71a8151c17cae24ac044c6ed0bfa3dda
bjblayney <bj.blayney@gmail.com>
2021-10-27 11:22:23 -0400

    trigger build

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi There! Updated.</h1>
+      <h1>Hi There! Updated. And Another.</h1>
       <hr />
       <MarketingApp />
     </div>
